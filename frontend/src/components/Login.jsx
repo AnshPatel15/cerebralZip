6a52724e150de77ab11cf9bd50355181cb7a7466
AnshPatel15 <anjalansh@gmail.com>
2025-03-05 19:27:16 +0530

    reverted

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -38,7 +38,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("/api/proxy?endpoint=login", {
+      const response = await fetch("http://3.111.196.92:8020/api/v1/login/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
